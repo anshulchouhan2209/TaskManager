870272322a1c56f64696328e4534ae1d7e76892e
Anshul Chouhan <chouhananshul2209@gmail.com>
2021-05-29 15:05:25 +0530

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <h1>
+                Task Manager
+                </h1>
+            
+        </div>
+    )
+}
+export default Header;
